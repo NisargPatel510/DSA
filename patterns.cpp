f32364b1cd099bc9614f48309bd3170b2c8836b2
NisargPatel510 <nisargpatel510@gmail.com>
2025-08-19 18:15:36 +0530

    update my patterns

--- patterns.cpp
+++ patterns.cpp
@@ -127,20 +127,164 @@ class patterns{
         }
         cout<<endl<<endl;
     }
-            void p11(int n){
+        void p11(int n){
         cout<<"Triangle of 01:\n";
+        int start=1;
+        for(int i=0;i<n;i++){
+            start=i%2==0?1:0;
+            for(int j=0;j<=i;j++){
+                cout<<start;
+                start=1-start;
+            }
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+    void p12(int n){
+        cout<<"Valley of num:\n";
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                if(j==i)
-                    cout<<1;
-                else if(j-i==1 || j-i==-1 || i-j==4 )
-                    cout<<0;
-                else if( j-i==2 || j-i==-2)
-                    cout<<1;
-                else if(i-j==3)
-                    cout<<0;
-                else if(i-j==4)
-                    cout<<6767;//no effect
+            int k=i;
+            for(int j=1;j<=2*n;j++){
+                if(j<=i)
+                    cout<<j;
+                else if(j>2*n-i){
+                    cout<<k--;
+                }
+                else
+                    cout<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+            void p13(int n){
+        cout<<"Triangle of nums:\n";
+        int k=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++)
+                cout<<k++;
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+            void p14(int n){
+        cout<<"Triangle of letters:\n";
+        for(int i=0;i<n;i++){
+            for(char ch='A'; ch<='A'+ i; ch++)
+                cout<<ch;
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+        void p15(int n){
+            cout<<"Inv Triangle of letters:\n";
+        for(int i=n;i>0;i--){
+            for(char ch='A'; ch<'A'+i; ch++)
+                cout<<ch;
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+            void p16(int n){
+            cout<<"Triangle of letters:\n";
+        for(int i=0;i<n;i++){
+            char ch='A'+i;
+            for(int j=0; j<=i;j++)
+                cout<<ch;
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+            void p17(int n){
+        int k,j;
+        cout<<"Pyramid of ABC:\n";
+        for(int i=0;i<n;i++){
+            for(k=0;k<n-i;k++)
+                cout<<" ";
+            int bp=(2*i+1)/2;
+            char ch='A';
+            for(j=0;j<2*i+1;j++){
+                cout<<ch;
+                if(j<=bp) ch++;
+                else
+                ch--;
+            }
+            for(k=0;k<n-i;k++)
+                cout<<" ";
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+        void p18(int n){
+        cout<<"Triangle of ABC:\n";
+        for(int i=0;i<n;i++){
+            char l='A' +n-1;
+            for(char ch=l-i;ch<=l;ch++){
+                cout<<ch;
+            }
+            cout<<endl;
+    }
+    cout<<endl<<endl;
+}
+    void p19(int n){
+        int inS=0,j;
+        for(int i=0;i<n;i++){
+            for(j=0;j<n-i;j++)
+                cout<<"*";
+            for(j=0;j<=inS;j++)
+                cout<<" ";
+            for(j=0;j<n-i;j++)
+                cout<<"*";
+            cout<<endl;
+            inS+=2;
+        }
+        inS-=2;
+        for(int i=0;i<n;i++){
+            for(j=0;j<=i;j++)
+                cout<<"*";
+            for(j=0;j<=inS;j++)
+                cout<<" ";
+            for(j=0;j<=i;j++)
+                cout<<"*";
+            cout<<endl;
+            inS-=2;
+        }
+        cout<<endl<<endl;
+        }
+    void p20(int n){
+        cout<<"Wing of star:\n";
+        int inS=2*n-2;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++)
+                cout<<"*";
+            for(int j=0;j<inS;j++)
+                cout<<" ";
+            for(int j=0;j<=i;j++)
+                cout<<"*";
+            inS-=2;
+            cout<<endl;
+        }
+        inS=2;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++)
+                cout<<"*";
+            for(int j=0;j<inS;j++)
+                cout<<" ";
+            for(int j=0;j<n-i-1;j++)
+                cout<<"*";
+            inS+=2;
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+        void p21(int n){
+        cout<<"Box of Star :\n";
+        int l=2*n-1;
+        for(int i=0;i<l;i++){
+            for(int j=0; j<=l;j++){
+                if(j==0 || j==l || i==0 || i==l)
+                    cout<<"4 ";
+                else cout<<"  ";
             }
             cout<<endl;
         }
@@ -160,11 +304,16 @@ int main(){
     np.p8(5);
     np.p9(5);
     np.p10(5);
-
-         
-
-
-
-
+    np.p11(5);
+    np.p12(5);
+    np.p13(5);
+    np.p14(5);
+    np.p15(5);
+    np.p16(5);
+    np.p17(5);
+    np.p18(5);
+    np.p19(5);
+    np.p20(5);
+    np.p21(5);
     return 0;
 }
